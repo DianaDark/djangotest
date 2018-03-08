99ce724b7d19ebbe5001fc4d38fd9c31cc797088
DianaDark <32294262+DianaDark@users.noreply.github.com>
2018-03-08 11:04:11 +0600

    employee, job

--- library_app/models.py
+++ library_app/models.py
@@ -1,51 +1,40 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
 
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField(max_length=1000)
     def __str__(self):
-        return self.name
 
-    class Meta:
-        db_table = "category"
-
-class Author(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.firstname + " " + self.lastname
-
-    class Meta:
-        db_table = "author"
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = "publisher"
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField()
-    isbn = models.CharField(max_length=20)
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE,
-                                  related_name="books")
-    authors = models.ManyToManyField(Author, related_name="books")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,
-                                 related_name="books")
-    def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+
+        return reverse('book-detail', args=[str(self.id)])
+
+
+import uuid
+
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+    job = models.ManyToManyField(Job)
+
+    STATUS = (
+        ('r', 'Rabotaet'),
+        ('u', 'Uvolen'),
+        ('o', 'otpusk'),
+    )
+
+    status = models.CharField(max_length=1, choices=STATUS, blank=True, default='d')
+
     class Meta:
-        db_table = "book"
+        ordering = ["birth_date"]
+
+    def __str__(self):
+        return '%s (%s)' % (self.id, self.book.title)
